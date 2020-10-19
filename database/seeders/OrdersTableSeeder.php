efecacc0690b5d2286f1afc08ce3c1adc44f4823
taif <taifbakshi.dev@gmail.com>
2020-10-19 16:14:29 +0600

    Updating data

---
+++ database/seeders/OrdersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Orders;
+
+class OrdersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 
+        //factory(App\Orders::class, 20)->create();
+        $this->call(App\Orders::class);
+    }
+}
